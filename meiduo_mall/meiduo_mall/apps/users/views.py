b2028a01d958bf5a5d857a1e4725fac8c138ff7b
潘灿 <1097218039@qq.com>
2020-04-20 16:45:17 +0800

    用户名重复判断接口

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+from users.models import User
+from django.http import JsonResponse
+
+
+class UsernameCountView(View):
+
+    def get(self, request, username):
+        '''接受用户名,判断是否存在'''
+
+        # 访问数据库
+        try:
+            count = User.objects.filter(username=username).count()
+        except Exception as e:
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '访问数据库失败'
+            })
+
+        return JsonResponse({
+            'code': 0,
+            'errmsg': 'ok',
+            'count': count
+        })
