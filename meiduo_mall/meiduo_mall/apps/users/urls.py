99961db761dc2d65a285f6a51b441c4266a6c970
潘灿 <1097218039@qq.com>
2020-04-24 11:35:23 +0800

    注册实现

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     re_path('^usernames/(?P<username>\w{5,20})/count/$',views.UsernameCountView.as_view()),
     re_path(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
+    re_path(r'^register/$', views.RegisterView.as_view())
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,9 +1,14 @@
+import json
+import re
+
+from django.contrib.auth import login
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
 from users.models import User
 from django.http import JsonResponse
+from django_redis import get_redis_connection
 
 
 class UsernameCountView(View):
@@ -42,4 +47,75 @@ class MobileCountView(View):
             'code': 0,
             'errmsg': 'ok',
             'count': count
+        })
+
+
+class RegisterView(View):
+    def post(self, request):
+        '''注册接口'''
+        dict = json.loads(request.body.decode())
+        username = dict.get('username')
+        password = dict.get('password')
+        password2 = dict.get('password2')
+        mobile = dict.get('mobile')
+        sms_code_client = dict.get('sms_code')
+        allow = dict.get('allow')
+
+        if not all(['username', 'password', 'password2', 'mobile', 'sms_code_client', 'allow']):
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '缺少必传参数'
+            })
+
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '用户名格式不正确'
+            })
+        if not re.match(r'^[a-zA-Z0-9]{8,20}$', password):
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '密码格式不正确'
+            })
+        if password2 != password:
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '两次输入密码不匹配'
+            })
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '手机号码格式不正确'
+            })
+        if not allow:
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '未勾选条款'
+            })
+        redis_coon = get_redis_connection('verify_code')
+        sms_code_server = redis_coon.get('sms_%s' % mobile)
+        if not sms_code_client:
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '短信验证码过期'
+            })
+        if sms_code_client != sms_code_server.decode():
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '验证码不匹配'
+            })
+
+        try:
+            user = User.objects.create_user(username=username,
+                                            password=password,
+                                            mobile=mobile)
+        except Exception as e:
+            return JsonResponse({
+                'code': 400,
+                'errmsg': '存入数据库失败'
+            })
+        login(request, user)
+        return JsonResponse({
+            'code': 0,
+            'errmsg': 'ok'
         })
