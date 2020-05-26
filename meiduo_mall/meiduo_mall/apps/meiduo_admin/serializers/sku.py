c4870c50580dc6d7c4a1e22d5b7d4a43317e13e2
潘灿 <1097218039@qq.com>
2020-05-26 11:38:36 +0800

    添加商品数据

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/sku.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/sku.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from goods.models import SKU, GoodsCategory, Goods, GoodsSpecification, SpecificationOption
+from goods.models import SKU, GoodsCategory, Goods, GoodsSpecification, SpecificationOption, SKUSpecification
 
 
 class SKUModelSerializer(serializers.ModelSerializer):
@@ -9,10 +9,32 @@ class SKUModelSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
+class SpecsSerializer(serializers.ModelSerializer):
+    spec_id = serializers.IntegerField()
+    option_id = serializers.IntegerField()
+
+    class Meta:
+        model = SKUSpecification
+        fields = ['spec_id', 'option_id']
+
+
 class SKUSerializer(serializers.ModelSerializer):
+    spu_id = serializers.IntegerField(label='商品id', write_only=True)
+    category_id = serializers.IntegerField(label='从属类别id', write_only=True)
+    spu = serializers.StringRelatedField(required=False)
+    category = serializers.StringRelatedField(required=False)
+    specs = SpecsSerializer(many=True)
+
     class Meta:
         model = SKU
         fields = '__all__'
+
+    def create(self, validated_data):
+        specs = validated_data.pop('specs')
+        sku = SKU.objects.create(**validated_data)
+        for spec in specs:
+            SKUSpecification.objects.create(sku=sku, **spec)
+        return sku
 
 
 class SKUThreeSerializer(serializers.ModelSerializer):
